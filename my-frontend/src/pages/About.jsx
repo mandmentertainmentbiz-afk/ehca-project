8786f9477eb2ada4224c274c6abd4bfcd1c971f4
mandmentertainmentbiz-afk <mandmentertainmentbiz@gmail.com>
2026-08-08 02:27:15 +0300

    Phase 4: Add website content editing

--- my-frontend/src/pages/About.jsx
+++ my-frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import SupportMissionForm from "../components/SupportMissionForm";
 import useSection from "../hooks/useSection";
+import DynamicSection from "../components/DynamicSection";
 
 export default function About() {
 
